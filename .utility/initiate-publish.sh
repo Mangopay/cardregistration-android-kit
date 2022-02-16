4911fcdd9b450d0d7c0c6484327c10b680511130
catalin.craciun <craciunctln@gmail.com>
2022-02-16 13:00:37 +0200

    Fixing gpg file location

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -2,4 +2,4 @@
 
 echo -e "Starting publish to Sonatype...\n"
 
-./gradlew publish -PnexusUsername="${MAVEN_USERNAME}" -PnexusPassword="${MAVEN_PASSWORD}" -Psigning.keyId=93605ADB -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=.utility/signingkey.gpg
+./gradlew publish -PnexusUsername="${MAVEN_USERNAME}" -PnexusPassword="${MAVEN_PASSWORD}" -Psigning.keyId=93605ADB -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=signingkey.gpg
